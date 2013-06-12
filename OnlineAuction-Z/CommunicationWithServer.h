a52dd56f4d1e67bded2cbd4e4d4f880137afb492
mh <zqzas1@gmail.com>
2013-06-12 19:01:15 +0800

    delegate: communication with server

---
+++ OnlineAuction-Z/CommunicationWithServer.h
@@ -0,0 +1,13 @@
+//
+//  CommunicationWithServer.h
+//  OnlineAuction-Z
+//
+//  Created by zqzas on 13-6-12.
+//  Copyright (c) 2013年 Hao Ma. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CommunicationWithServer : NSObject
+
+@end
---
+++ OnlineAuction-Z/CommunicationWithServer.m
@@ -0,0 +1,13 @@
+//
+//  CommunicationWithServer.m
+//  OnlineAuction-Z
+//
+//  Created by zqzas on 13-6-12.
+//  Copyright (c) 2013年 Hao Ma. All rights reserved.
+//
+
+#import "CommunicationWithServer.h"
+
+@implementation CommunicationWithServer
+
+@end
